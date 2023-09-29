510fd46b90553468861515fe5bf1b36f736a9383
Vika <143884038+VikaaGracheva@users.noreply.github.com>
2023-09-29 21:38:31 +0000

    lab_2.2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include <stdio.h>
 
-void RevertString(char *str)
+void RevertString(char s[])
 {
-	// your code here
+int i,new;
+int swap = 0;
+for(new=0; s[new] != 0; new++);
+for(i=0; i<new/2; i++)
+{
+    swap = s[i];
+    s[i] = s[new - 1 - i];
+    s[new -1 -i] = swap;
 }
+for (i = 0; i<new; i++)
+    putchar(s[i]);
+}	
+
 
